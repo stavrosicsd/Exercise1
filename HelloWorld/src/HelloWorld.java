a19bfbe86cf0a487f1ac3efe83751128d41644d6
stavrosicsd <stavros_icsd06042@hotmail.com>
2016-10-23 13:28:03 +0300

    Fix master after merge

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -14,16 +14,12 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello ASE2016 - how are you now? :)");
-=======
-		System.out.println("Hello ASE2016 - how are you? :)");
-		
+
 	    Scanner scanner = new Scanner(System.in);
 	    System.out.print("Please enter your name: ");
 
 	    System.out.println(new HelloUser(scanner.next()).greetUser());
->>>>>>> hello_user
 	}
 
 }
